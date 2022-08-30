37e77b976a32b4148ac61ea2e4c7bd8e14f81957
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-08-31 00:55:36 +0200

    Course information: step5 (Exercise 1.5)

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,7 @@
 const Header = (props) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>)
 }
 
@@ -17,19 +17,21 @@ const Part = (props) => {
 
 const Content = (props) => {
   //console.log("Contents: ", props);
+  let parts = props.course.parts;
   return (
     <>
-      <Part name={props.parts[0].name} number={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} number={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} number={props.parts[2].exercises} />
+      <Part name={parts[0].name} number={parts[0].exercises} />
+      <Part name={parts[1].name} number={parts[1].exercises} />
+      <Part name={parts[2].name} number={parts[2].exercises} />
     </>
   )
 }
 
 const Total = (props) => {
   let total = 0;
-  props.parts.map((val, key) => {
+  props.course.parts.map((val, key) => {
     total += val.exercises;
+    return total;
   });
   return (
     <>
@@ -39,27 +41,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <p>Number of exercises: <Total parts={parts} /></p>
+      <Content course={course} />
+      <p>Number of exercises: <Total course={course} /></p>
     </>
   )
 }
