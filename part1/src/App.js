1fca08869cae82074a031d1285c44a84a348b9d5
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-09-02 23:39:00 +0200

    JS destructuring in practice

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,71 +1,29 @@
-const Header = (props) => {
+const Hello = ({age,name}) => {
+  
+  const bornYear = () => new Date().getFullYear() - age;
   return (
-    <>
-      <h1>{props.course.name}</h1>
-    </>)
-}
-
-const Part = (props) => {
-  return (
-    <>
+    <div>
       <p>
-        {props.name} {props.number}
+        Hello {name}, you are {age} years old
       </p>
-    </>
-  )
-}
-
-const Content = (props) => {
-  //console.log("Contents: ", props);
-  let parts = props.course.parts;
-  return (
-    <>
-      <Part name={parts[0].name} number={parts[0].exercises} />
-      <Part name={parts[1].name} number={parts[1].exercises} />
-      <Part name={parts[2].name} number={parts[2].exercises} />
-    </>
-  )
-}
-
-const Total = (props) => {
-  let total = 0;
-  props.course.parts.map((val, key) => {
-    total += val.exercises;
-    return total;
-  });
-  return (
-    <>
-      {total}
-    </>
+      <p>
+        So you probably born in {bornYear()}
+      </p>
+    </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const name = 'Peter'
+  const age = 10
 
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <p>Number of exercises: <Total course={course} /></p>
-    </>
+  return ( 
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
   )
 }
 
-export default App
+export default App;
