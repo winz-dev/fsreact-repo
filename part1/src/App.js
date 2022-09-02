30f7c078655f6ffd7a4cd16a4495f0e275e5067c
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-09-02 23:58:32 +0200

    Page re-rendering: example with counter system.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,29 +1,7 @@
-const Hello = ({age,name}) => {
-  
-  const bornYear = () => new Date().getFullYear() - age;
+const App = (props) => {
+  const {counter} = props;
   return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>
-        So you probably born in {bornYear()}
-      </p>
-    </div>
+    <div>{counter}</div>
   )
 }
-
-const App = () => {
-  const name = 'Peter'
-  const age = 10
-
-  return ( 
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  )
-}
-
-export default App;
+export default App; 
