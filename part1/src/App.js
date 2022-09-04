df26e39626dcb2f4969a07671cfccbedbebe305b
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-09-04 05:29:28 +0200

    Refactoring the components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,21 +1,21 @@
 import { useState } from 'react'
 
-const Display = (props) => (<>{props.counter}</>)
-const Button = (props) => (<button onClick={props.onClick}>{props.text}</button>)
+const Display = ({counter}) => (<>{counter}</>)
+const Button = ({text, onClick}) => (<button onClick={onClick}>{text}</button>)
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  const setToZero = () => setCounter(0)
+  const setToZero = () => setCounter(0) 
   const addOne = () => setCounter(counter + 1)
   const decreaseByOne = () => setCounter(counter - 1)
 
   return (
-    <div>
+    <>
       <Display counter={counter} /> <br />
       <Button text="Reset" onClick={setToZero} />
       <Button text="Plus" onClick={addOne} />
       <Button text="Minus" onClick={decreaseByOne} />
-    </div>
+    </>
   )
 }
 export default App;  
