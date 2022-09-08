244bc43f812631b6c8783f8da43e664191c5c2e5
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-09-08 04:00:32 +0200

    Complex state -- Object spread operator -- React don't allow direct object state modification (mutation) to avoid unexpected side effects

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,16 +1,28 @@
 import { useState } from "react"
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+ 
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0
+  })
 
-  const clickLeft = () => setLeft(left + 1)
-  const clickRight = () => setRight(right + 1)
+  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
+  
+  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
+  
+  /*const handleLeftClick = () => {
+    const newClick = {
+      left : clicks.left + 1,
+      right : clicks.right
+    }  
+    setClicks(newClick)
+  }*/
 
   return (
     <>
-      <button onClick={clickLeft}>  Left => <strong>{left}</strong></button>
-      <button onClick={clickRight}> Right =><strong>{right}</strong></button>
+      <button onClick={handleLeftClick}>  Left => <strong>{clicks.left}</strong></button>
+      <button onClick={handleRightClick}> Right => <strong>{clicks.right}</strong></button>
     </>)
 }
 
