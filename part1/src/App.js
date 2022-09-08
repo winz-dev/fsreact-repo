f07825234398719bdb2de9103924bff514dce352
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-09-08 04:45:26 +0200

    Handling arrays

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,25 +4,19 @@ const App = () => {
  
   const [clicks, setClicks] = useState({
     left: 0,
-    right: 0
+    right: 0,
+    allClicks: []
   })
 
-  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1})
+  const handleRightClick = () => setClicks({...clicks, right: clicks.right + 1, allClicks: clicks.allClicks.concat('R')})
   
-  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1})
+  const handleLeftClick = () => setClicks({...clicks, left: clicks.left + 1, allClicks: clicks.allClicks.concat('L')})
   
-  /*const handleLeftClick = () => {
-    const newClick = {
-      left : clicks.left + 1,
-      right : clicks.right
-    }  
-    setClicks(newClick)
-  }*/
-
   return (
     <>
       <button onClick={handleLeftClick}>  Left => <strong>{clicks.left}</strong></button>
       <button onClick={handleRightClick}> Right => <strong>{clicks.right}</strong></button>
+      <p>{clicks.allClicks.join('-')}</p>
     </>)
 }
 
