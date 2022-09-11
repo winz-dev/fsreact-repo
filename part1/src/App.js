6cd2523bbb044b971dd2444b3a01e60ee4354c64
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-09-11 04:41:43 +0200

    Part 1 (Unicafe):  step5 (exercise 1.10)

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const StatisticLine = ({name, value}) => {
+  return (
+    <>
+      <li>{name}: {value}</li>
+    </>
+  )
+}
+
 const Statistic = ({ good, neutral, bad }) => {
   let positive = 0;
   let allFeeds = good + neutral + bad
@@ -11,12 +19,12 @@ const Statistic = ({ good, neutral, bad }) => {
       <>
         <h2>Statistics</h2>
         <ul>
-          <li>Good: {good}</li>
-          <li>Neutral: {neutral}</li>
-          <li>Bad: {bad}</li>
-          <li>All: {allFeeds}</li>
-          <li>Positive: {positive}</li>
-          <li>Average: {(good + neutral + bad) / 3}</li>
+          <StatisticLine name="Good" value={good} />
+          <StatisticLine name="Neutral" value={neutral} />
+          <StatisticLine name="Bad" value={bad} />
+          <StatisticLine name="All" value={allFeeds} />
+          <StatisticLine name="Positive" value={positive} />
+          <StatisticLine name="Average" value={(allFeeds / 3)} />
         </ul>
       </>
     )
@@ -27,20 +35,29 @@ const Statistic = ({ good, neutral, bad }) => {
     </>)
 }
 
+const Button = ({handClickEvent, text}) => {
+
+  return (
+    <>
+      <button onClick={handClickEvent}>{text}</button>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const incValue = (val) => ((val + 1))
   return (
     <div>
       <h1>Unicafe</h1>
       <hr />
       <h2>Give Feedback</h2>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Button handClickEvent={() => incValue(setGood(good + 1))} text="Good" />
+      <Button handClickEvent={() => incValue(setNeutral(neutral + 1))} text="Bad" />
+      <Button handClickEvent={() => incValue(setBad(bad + 1))} text="Bad" />
       <Statistic good={good} neutral={neutral} bad={bad} />
     </div>
   )
