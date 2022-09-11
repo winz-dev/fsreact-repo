e9bb37e5d23704a18e1083825febe9c23ddd02e5
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-09-11 03:45:46 +0200

    Part 1 (Unicafe):  step2 (exercise 1.7)

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # fsreact-repo
 Project Full Stack web dev
-Exercice submission repo
+Exercice submission to repo
 Part 0:
 Exercises submitted:
 Part 1
 # Course information
---  From Exercise 1.1 to Exercise 1.5
- Commit details: https://github.com/winz-dev/fsreact-repo/pull/3
+-- From Exercise 1.1 to Exercise 1.5
+-- Commit details: https://github.com/winz-dev/fsreact-repo/pull/3
 # Unicafe
--- Init step (Exercise 1.6)   
+-- Init step (Exercise 1.6)
 
 
 
--- part1/src/App.js
+++ part1/src/App.js
@@ -8,7 +8,22 @@ const App = () => {
 
   return (
     <div>
-      Start here
+      <h1>Unicafe</h1>
+      <hr />
+      <h2>Give Feedback</h2>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <br />
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>All: {good + neutral + bad}</li>
+        <li>Positive: {good / (good + neutral + bad)}</li>
+        <li>Average: {(good + neutral + bad) / 3}</li>
+      </ul>
     </div>
   )
 }
