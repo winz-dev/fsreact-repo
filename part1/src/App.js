9017a81483122b9bdb52f6dc8802fd075fd42974
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-09-15 23:42:16 +0200

    Part 1 (Anecdotes):  step2 (exercise 1.13)

--- part1/src/App.js
+++ part1/src/App.js
@@ -10,15 +10,36 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ]
-   
+
   const [selected, setSelected] = useState(0)
-  
-  const handleClick = () => (setSelected(Math.floor(Math.random() * anecdotes.length)));
+  const [votes, setVotes] = useState({})
+  let index = selected;
+
+  const addVotes = () => {
+    let obj = { ...votes }
+    index = selected.toString()
+
+    if (!obj.hasOwnProperty(index)) {
+      obj[index] = 1
+      return setVotes(obj)
+    }
+    obj[index]++
+    return setVotes(obj)
+  }
+
+  const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+
+  console.log(votes);
 
   return (
     <div>
-      {anecdotes[selected]} <br />
+      <h1>Anecdote</h1>
+      <p>{anecdotes[selected]} <br />
+      has <strong>{votes[selected]}</strong> votes
+      </p>
+      <button onClick={addVotes}>votes</button>
       <button onClick={handleClick}>next anecdote</button>
+      <hr />
     </div>
   )
 }
