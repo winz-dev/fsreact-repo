50b495da87bc5327baba9a3c90cc3e156afe9948
Delphin Koumouna SAGNO <DSA21279@open-groupe.com>
2022-09-21 04:38:56 +0200

    Part 2 (Course information) step9 (exercise 2.4)

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -1,55 +1,69 @@
-const Course = (props) => {
-  const sum = props.course.parts.reduce((total, currentVal) => {
-    console.log('total: ', total, '\n CurrentValue: ', currentVal)
+const Course = ({ course }) => {
+  const sum = course.parts.reduce((total, currentVal) => {
     return total + currentVal.exercises
   }, 0)
+
+  return (
+    <div>
+      <h2>{course.name}</h2>
+      {course.parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)}
+      Total of exercises is: <strong>{sum}</strong> <br />
+    </div>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <>
       <h1>Course information</h1>
       <hr />
-      <h2>{props.course.name}</h2>
-      {props.course.parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)}
-      <br />
-      Total of exercises is: <strong>{sum}</strong>
+      {courses.map(course => <Course course={course} key={course.id} />)}
     </>
   )
 }
-
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 7,
-        id: 4
-      },
-      {
-        name: 'React Native',
-        exercises: 25,
-        id: 5
-      }
-    ]
-  }
-
-  return <Course course={course} />
-}
-
 export default App
